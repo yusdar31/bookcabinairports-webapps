b5abb2052e76fd3e3dcd19647376208948a96ba0
Andi Yusdar Al Imran <andi.yusdar@mastersystem.co.id>
2026-04-20 16:56:09 +0700

    fix laravel test bootstrap class

---
+++ app/tests/TestCase.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Tests;
+
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+
+abstract class TestCase extends BaseTestCase
+{
+    // Laravel 13 can bootstrap the application directly from the base test case.
+}
